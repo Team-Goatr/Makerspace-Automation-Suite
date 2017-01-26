96f0252a6368470596cb191b2b515971fe3f28dd
dkoutsky3 <dkoutsky3@gatech.edu>
2017-01-25 23:08:23 -0500

    no content admin page within exisiting wp frame

--- admin-page.php
+++ admin-page.php
@@ -1,19 +1,38 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>My WordPress Plugin Front-end Page</title>
-<?php
-//call the wp head so  you can get most of your wordpress
-wp_head();
-?>
-</head>
-<body>
-<h1>Here's my plugin front-end page</h1>
-<h2>You can put anything you want in this php file.</h2>
-<?php
-//call the wp foooter
-wp_footer();
-?>
-</body>
+<?php /* Template Name: CustomPageT1 */ ?>
+
+<?php get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			// Include the page content template.
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) {
+				comments_template();
+			}
+
+			// End of the loop.
+		endwhile;
+		?>
+
+	</main><!-- .site-main -->
+
+	<?php get_sidebar( 'content-bottom' ); ?>
+
+</div>
+
+<html> <!-- JASMINE AND DANA's STUFF -->
+    <body>
+        <h1>Here's my plugin front-end page</h1>
+        <h2>You can put anything you want in this php file.</h2>
+    </body>
 </html>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
+
