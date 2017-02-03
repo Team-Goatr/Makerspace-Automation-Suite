c2dc3c2b9c955d9231f2d5f1b48803630a92a8d0
Jasmine <jbrown@onetrust.com>
2017-02-02 19:05:00 -0500

    file structure + mas classes

--- front-end-pages/admin-pages/members-tab/member-table/admin-members-table.php
+++ front-end-pages/admin-pages/members-tab/member-table/admin-members-table.php
@@ -8,18 +8,14 @@
 		<?php
 		// Start the loop.
 		while ( have_posts() ) : the_post();
-
 			// Include the page content template.
 			get_template_part( 'template-parts/content', 'page' );
-
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) {
 				comments_template();
 			}
-
 			// End of the loop.
 		endwhile;
-
 		?>
 
 	</main><!-- .site-main -->
@@ -30,4 +26,3 @@
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
-
