c89c5f62a1a32c82f5069f5eac973147d0691369
Thomas Coe <thomascoe1@gmail.com>
2017-02-09 16:00:42 -0500

    Additional register.php and form updates

--- front-end-pages/admin-pages/test/register.php
+++ front-end-pages/admin-pages/test/register.php
@@ -1,19 +1,41 @@
 <?php
 include dirname(__DIR__).'/resources/StripeAPI.php';
 
+echo "<html><body>\n";
+
 // Dump everything (for testing)
 print_r($_POST);
+echo "<br>";
 
 // Get things submitted to the form
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
 $token = $_POST['stripeToken'];
 $email = $_POST['stripeEmail'];
+$type = $_POST['type'];
+$recurring = $_POST['recurring'];
+
+// Get plan from Stripe
+$plan = retrieveStripePlan($type);
+if (empty($plan)) {
+    echo "Invalid subscription type.";
+    die();
+}
 
 // Create a new customer
 $customer = createStripeCustomer($email, $token);
-
+echo "Customer $firstname $lastname created with ID: " . $customer->id . "<br>";
 // TODO: The $customer->id needs to be stored in G Suite
 
-// Subscribe the new customer as basic individual
-$charge = subscribeStripeCustomer($customer->id, "basic-ind");
+if ($recurring == 'true') {
+    // Subscribe the new customer as basic individual
+    subscribeStripeCustomer($customer->id, $type);
+    echo "Customer $email has been subscribed to " . $plan->name . "<br>";
+} else {
+    $charge = chargeStripeCustomer($customer->id, $plan->amount);
+    echo "Customer $email has been charged for " . $plan->name . "<br>";
+}
+
+echo "</body></html>";
 
 ?>
