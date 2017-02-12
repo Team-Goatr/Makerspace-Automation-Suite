f54bf17fabada260a70cc20efbadc6ea971c23e0
Thomas Coe <thomascoe1@gmail.com>
2017-02-12 13:00:56 -0500

    reg form

--- front-end-pages/registration-form/registration-form.php
+++ front-end-pages/registration-form/registration-form.php
@@ -3,6 +3,7 @@
     <main id="main" class="site-main" role="main">
     <?php include 'registration-form.html';?>
     </main><!-- .site-main -->
+    <?php get_sidebar( 'content-bottom' ); ?>
 </div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
