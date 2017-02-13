14cb36d70b4fc134b2d248990aac9cec1ca173f9
Thomas Coe <thomascoe1@gmail.com>
2017-02-13 18:58:55 -0500

    Move resources directory, fix PHP errors in registration form (#51)
    * Update Stripe function to accept email and amt
    
    * New Stripe functions for getting plan and customer
    
    * move resources folder outside of admin-pages
    
    * registration form updates (confirmation page, Stripe integration)

--- front-end-pages/registration-form/confirm.php
+++ front-end-pages/registration-form/confirm.php
@@ -1,69 +1,94 @@
-        <br>
+<?php
+include dirname(__DIR__).'/resources/StripeAPI.php';
 
+//get_header();
+
+//$submitfile = plugins_url('submit.php', __FILE__);
+$submitfile = 'submit.php';
+
+// Things that were POST'ed
+$first = $_POST["firstname"];
+$last = $_POST["lastname"];
+$email = $_POST["email"];
+$username = $_POST["username"] . "@decaturmakers.org";
+$type = $_POST['type'];
+$checked = empty($_POST["autorenew"]) ? "" : "checked";
+
+// TODO: Check G suite if this username already exists. If so, print warning and don't allow user to continue
+
+// Get plan from Stripe
+$plan = retrieveStripePlan($type);
+if (empty($plan)) {
+    echo "Invalid subscription type.";
+    die();
+}
+
+$planamount = $plan->amount;
+$planname = $plan->name;
+$plandollars = $planamount/100;
+
+echo <<<END
+
+<html>
+    <body>
+        <h1>Registration Confirmation Page</h1>
         <div class="row">
-            <?php
-            echo '<form action="' . plugins_url( 'confirm.php', __FILE__ ) . '" method="POST" class="form-horizonal">';
-            ?>
+            <form action="$submitfile" method="POST" class="form-horizonal">
                 <div class="form-group">
                     <label for="first" class="col-md-3 control-label">First Name:</label>
                     <div class="col-sm-10">
-                        <input type="text" style="width: 60%" class="form-control" id="first" name="firstname" placeholder="">
+                        <input type="text" style="width: 60%" class="form-control" id="first" name="firstname" value="$first" readonly="readonly">
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label for="last" class="col-md-3 control-label">Last Name:</label>
                     <div class="col-sm-10">
-                        <input type="text" style="width: 60%" class="form-control" id="last" name="lastname" placeholder="">
+                        <input type="text" style="width: 60%" class="form-control" id="last" name="lastname" value="$last" readonly="readonly">
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label for="email" class="col-md-3 control-label">Email:</label>
                     <div class="col-sm-10">
-                        <input type="email" style="width: 60%" class="col-md-6 form-control" id="email" name="email" placeholder="">
+                        <input type="email" style="width: 60%" class="col-md-6 form-control" id="email" name="email" value="$email" readonly="readonly">
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label for="username" class="col-md-3 control-label" >Username:</label>
                     <div class="col-sm-10">
-                        <input type="text" style="width: 30%" class="col-md-6 form-control" id="username" name="username" placeholder="">
-                        @decaturmakers.org
+                        <input type="text" style="width: 60%" class="col-md-6 form-control" id="username" name="username" value="$username" readonly="readonly">
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label for="type" class="col-md-3 control-label" >Membership type:</label>
                     <div class="col-sm-10">
-                        <select style="width: 25%" class="form-control" name="type" id="type">
-                            <option value="basic-ind">Individual Basic ($25/mo)</option>
-                            <option value="support-ind">Individual Supporter ($40/mo)</option>
-                            <option value="basic-fam">Family Basic ($50/mo)</option>
-                            <option value="support-fam">Family Supporter ($75/mo)</option>
-                            <option value="invalid">Invalid</option>
-                        </select>
+                        <label>$planname for $plandollars/mo</label>
                     </div>
                 </div>
 
                 <div class="form-group">
                     <div class="col-sm-10">
                         <div class="checkbox">
-                            <label><input type="checkbox" name="autorenew" checked> Auto-renew Membership</label>
+                            <label><input type="checkbox" name="autorenew" $checked> Auto-renew Membership</label>
                         </div>
                     </div>
                 </div>
 
+                <input type="hidden" value="$type" name="type" />
+
                 <div class="form-group">
                     <div class="col-xs-offset-2 col-xs-10">
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <!--<button type="submit" class="btn btn-primary">Submit</button>-->
+END;
+                        getStripeCheckout($email, $planamount);
+echo <<<END
                     </div>
                 </div>
-
             </form>
         </div>
-
-        <!--<p style="font-style: italic">Payments have been secured with Stripe</p>-->
 
         <!-- ANGULAR -->
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>
@@ -74,3 +99,11 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
+</html>
+
+
+END;
+
+//get_footer();
+?>
--- front-end-pages/registration-form/registration-form.php
+++ front-end-pages/registration-form/registration-form.php
@@ -1,36 +1,9 @@
-<?php /* Template Name: CustomPageT1 */ ?>
-
-<?php include 'registration-form.html';?>
-
+<?php get_header(); ?>
 <div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
-		<?php
-		// Start the loop.
-		/*while ( have_posts() ) : the_post();
-
-			// Include the page content template.
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) {
-				comments_template();
-			}
-
-			// End of the loop.
-		endwhile;*/
-
-		// changes the html in the div based on button click
-		switch($_GET['content']) {
-			case 1: include 'members-tab/member-table/admin-members-table.html'; break;
-            case 3: include 'access-tab/admin-access.html'; break;
-			case 4: include 'integrations-tab/admin-integrations.html'; break;
-			case 5: include 'members-tab/member-edit/admin-members-edit.html'; break;
-			case 6: include 'test/form.html'; break;
-            		default: include 'members-tab/member-table/admin-members-table.html'; break;
-		}
-		
-		?>
-
-	</main><!-- .site-main -->
-
+    <main id="main" class="site-main" role="main">
+    <?php include 'registration-form.html';?>
+    </main><!-- .site-main -->
+    <?php get_sidebar( 'content-bottom' ); ?>
 </div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
