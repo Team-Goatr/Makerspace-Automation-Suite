7f70a3c8780d19ce2fec40d71c50710965795e05
Craig Owenby <craig.owenby@gatech.edu>
2017-02-14 18:01:54 -0500

    Bugfixes

--- front-end-pages/admin-pages/integrations-tab/integrations-manager.php
+++ front-end-pages/admin-pages/integrations-tab/integrations-manager.php
@@ -4,15 +4,15 @@ include dirname(__DIR__).'/resources/GSuiteAPI.php';
 include dirname(__DIR__).'/resources/StripeAPI.php';
 
 if (isset($_POST["stripe-public"])) {
-    updateStripePublic($_GET["stripe-public"]);
+    updateStripePublic($_POST["stripe-public"]);
 }
 
 if (isset($_POST["stripe-private"])) {
-    updateStripeSecret($_GET["stripe-private"]);
+    updateStripeSecret($_POST["stripe-private"]);
 }
 
 if (isset($_POST["gsuite-json"])) {
-    updateGSuiteCredentials($_GET["gsuite-json"]);
+    updateGSuiteCredentials($_POST["gsuite-json"]);
 }
 
 echo '<script>window.location = "https://goatr.tech/wp-admin/admin.php?page=mas-plugin&content=4"</script>';
