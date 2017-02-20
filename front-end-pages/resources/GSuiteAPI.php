0d469af1bef7de42691d6b40e335810c3a6cde42
Ryan Strat <ryanstrat@users.noreply.github.com>
2017-02-19 22:42:47 -0500

    removing fields for testing

--- front-end-pages/resources/GSuiteAPI.php
+++ front-end-pages/resources/GSuiteAPI.php
@@ -72,8 +72,8 @@ function userFactory($username, $email, $firstName, $lastName, $password, $strip
             'Subscription_Management' => array(
                 'Subscription_Type' => $subcriptionType,
                 'Subscription_Status' => $subscriptionStatus,
-                'Subscription_Recurring' => $subscriptionRecurring,
-                'Subscription_Expiration' => $subscriptionExpiration,
+                //'Subscription_Recurring' => $subscriptionRecurring,
+                //'Subscription_Expiration' => $subscriptionExpiration,
                 'Stripe_ID' => $stripeToken
                 ) 
             )
