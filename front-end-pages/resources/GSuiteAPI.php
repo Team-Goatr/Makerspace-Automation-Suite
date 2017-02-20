8c13e455c2545470e13661cd84ac4460971f94ff
Craig Owenby <craig.owenby@gatech.edu>
2017-02-19 22:31:13 -0500

    Debug output

--- front-end-pages/resources/GSuiteAPI.php
+++ front-end-pages/resources/GSuiteAPI.php
@@ -78,6 +78,8 @@ function userFactory($username, $email, $firstName, $lastName, $password, $strip
                 ) 
             )
     );
+    error_log(var_dump($userData));
+    
     $user = new Google_Service_Directory_User($userData);
     return $user;
 }
