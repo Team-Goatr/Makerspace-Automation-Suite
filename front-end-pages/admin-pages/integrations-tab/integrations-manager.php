ae006f0a60504f93eba316ce06c29fb608ef6d08
Craig Owenby <craig.owenby@gatech.edu>
2017-02-21 21:48:16 -0500

    Attempting to prevent access to PHP files from the browser

--- front-end-pages/admin-pages/integrations-tab/integrations-manager.php
+++ front-end-pages/admin-pages/integrations-tab/integrations-manager.php
@@ -4,7 +4,20 @@
 add_action('admin_post_update_keys', 'prefix_admin_update_keys');
 
 function prefix_admin_update_keys() {
-    echo 'Test!';
-}
+    include dirname(__DIR__).'/../resources/GSuiteAPI.php';
+    include dirname(__DIR__).'/../resources/StripeAPI.php';
 
-echo 'Butts';
+    if (isset($_POST["stripe-public"])) {
+        updateStripePublic($_POST["stripe-public"]);
+    }
+
+    if (isset($_POST["stripe-private"])) {
+        updateStripeSecret($_POST["stripe-private"]);
+    }
+
+    if (isset($_POST["gsuite-json"])) {
+        updateGSuiteCredentials($_POST["gsuite-json"]);
+    }
+
+    wp_redirect(admin_url('admin.php?page=mas-plugin&content=4)"</script>'));
+}
