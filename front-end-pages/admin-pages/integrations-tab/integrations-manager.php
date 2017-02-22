9207b44773a510a0f7ce262fe01a54f80dcaf861
Craig Owenby <craig.owenby@gatech.edu>
2017-02-22 14:17:20 -0500

    Attempting to prevent access to PHP files from the browser

--- front-end-pages/admin-pages/integrations-tab/integrations-manager.php
+++ front-end-pages/admin-pages/integrations-tab/integrations-manager.php
@@ -1,12 +1,12 @@
 <?php
 
+include dirname(__DIR__).'/../resources/GSuiteAPI.php';
+include dirname(__DIR__).'/../resources/StripeAPI.php';
+
 // Adding the keys to the wordpress hooks
 add_action('admin_post_update_keys', 'prefix_admin_update_keys');
 
 function prefix_admin_update_keys() {
-    include dirname(__DIR__).'/../resources/GSuiteAPI.php';
-    include dirname(__DIR__).'/../resources/StripeAPI.php';
-
     if (isset($_POST["stripe-public"])) {
         updateStripePublic($_POST["stripe-public"]);
     }
