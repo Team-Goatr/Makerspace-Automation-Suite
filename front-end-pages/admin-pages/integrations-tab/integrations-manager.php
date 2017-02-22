37d4d740b42b7fb6afc407a2c47378c28ddd9220
Craig Owenby <craig.owenby@gatech.edu>
2017-02-22 14:41:30 -0500

    Attempting to prevent access to PHP files from the browser

--- front-end-pages/admin-pages/integrations-tab/integrations-manager.php
+++ front-end-pages/admin-pages/integrations-tab/integrations-manager.php
@@ -7,17 +7,17 @@ include_once dirname(__DIR__).'/../resources/StripeAPI.php';
 add_action('admin_post_update_keys', 'prefix_admin_update_keys');
 
 function prefix_admin_update_keys() {
-    if (isset($_POST["stripe-public"])) {
-        updateStripePublic($_POST["stripe-public"]);
-    }
-
-    if (isset($_POST["stripe-private"])) {
-        updateStripeSecret($_POST["stripe-private"]);
-    }
-
-    if (isset($_POST["gsuite-json"])) {
-        updateGSuiteCredentials($_POST["gsuite-json"]);
-    }
+//    if (isset($_POST["stripe-public"])) {
+//        updateStripePublic($_POST["stripe-public"]);
+//    }
+//
+//    if (isset($_POST["stripe-private"])) {
+//        updateStripeSecret($_POST["stripe-private"]);
+//    }
+//
+//    if (isset($_POST["gsuite-json"])) {
+//        updateGSuiteCredentials($_POST["gsuite-json"]);
+//    }
     
     echo '<h1>Success</h1>';
 
