2fedec1057843ffeab2222325731a7587b4a8d97
Craig Owenby <craig.owenby@gatech.edu>
2017-02-25 17:23:41 -0500

    Attempting to use wordpress admin-post to handle POST requests

--- postable/integrations-manager.php
+++ postable/integrations-manager.php
@@ -1,7 +1,7 @@
 <?php
 
-include_once dirname(__DIR__).'/../resources/GSuiteAPI.php';
-include_once dirname(__DIR__).'/../resources/StripeAPI.php';
+include_once dirname(__DIR__).'/../front-end-pages/resources/GSuiteAPI.php';
+include_once dirname(__DIR__).'/../front-end-pages/resources/StripeAPI.php';
 
 // Adding the keys to the wordpress hooks
 add_action('admin_post_update_keys', 'prefix_admin_update_keys');
