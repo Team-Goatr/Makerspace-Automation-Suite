27eba7cee94c8fbf39c6b9509e9ccc5ab56ba95c
Craig Owenby <craig.owenby@gatech.edu>
2017-02-25 17:03:13 -0500

    Attempting to use wordpress admin-post to handle POST requests

--- front-end-pages/admin-pages/integrations-tab/integrations-manager.php
+++ front-end-pages/admin-pages/integrations-tab/integrations-manager.php
@@ -1,39 +1,31 @@
 <?php
 
-add_action( 'admin_post_add_foobar', 'prefix_admin_add_foobar' );
-//this next action version allows users not logged in to submit requests
+include_once dirname(__DIR__).'/../resources/GSuiteAPI.php';
+include_once dirname(__DIR__).'/../resources/StripeAPI.php';
 
-//if you want to have both logged in and not logged in users submitting, you have to add both actions!
+// Adding the keys to the wordpress hooks
+add_action('admin_post_update_keys', 'prefix_admin_update_keys');
 
-add_action( 'admin_post_nopriv_add_foobar', 'prefix_admin_add_foobar' );
+add_action('admin_post_test', 'prefix_admin_test')
 
+function prefix_admin_update_keys() {
+    if (isset($_POST["stripe-public"])) {
+        updateStripePublic($_POST["stripe-public"]);
+    }
 
-function prefix_admin_add_foobar() {
-    status_header(200);
-    die("Server received '{$_REQUEST['data']}' from your browser.");
-    //request handlers should die() when they complete their task
+    if (isset($_POST["stripe-private"])) {
+        updateStripeSecret($_POST["stripe-private"]);
+    }
+
+    if (isset($_POST["gsuite-json"])) {
+        updateGSuiteCredentials($_POST["gsuite-json"]);
+    }
+    
+    echo '<h1>Success</h1>';
+
+    wp_redirect(admin_url('admin.php?page=mas-plugin&content=4'));
 }
 
-//include_once dirname(__DIR__).'/../resources/GSuiteAPI.php';
-//include_once dirname(__DIR__).'/../resources/StripeAPI.php';
-//
-//// Adding the keys to the wordpress hooks
-//add_action('admin_post_update_keys', 'prefix_admin_update_keys');
-//
-//function prefix_admin_update_keys() {
-//    if (isset($_POST["stripe-public"])) {
-//        updateStripePublic($_POST["stripe-public"]);
-//    }
-//
-//    if (isset($_POST["stripe-private"])) {
-//        updateStripeSecret($_POST["stripe-private"]);
-//    }
-//
-//    if (isset($_POST["gsuite-json"])) {
-//        updateGSuiteCredentials($_POST["gsuite-json"]);
-//    }
-//    
-//    echo '<h1>Success</h1>';
-//
-//    wp_redirect(admin_url('admin.php?page=mas-plugin&content=4'));
-//}
+function prefix_admin_keys() {
+    die('Working!');
+}
