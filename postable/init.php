024c83e11bd564ca2a9e1f00ce0c8feb31ead080
Craig Owenby <craig.owenby@gatech.edu>
2017-03-08 21:24:57 -0500

    Restored previous registration form functionality

--- postable/init.php
+++ postable/init.php
@@ -5,5 +5,4 @@ defined( 'ABSPATH' ) or die();
 
 // Including postable actions
 include_once plugin_dir_path( __FILE__ ) . 'integrations-manager.php';
-include_once plugin_dir_path( __FILE__ ) . 'registration.php';
 include_once plugin_dir_path( __FILE__ ) . 'update-card.php';
