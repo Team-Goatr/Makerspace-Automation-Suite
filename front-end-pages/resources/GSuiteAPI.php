eccddbbcb367d51578b8ed1a5ae00962ea0f7860
Ryan Strat <ryanstrat@users.noreply.github.com>
2017-03-11 02:00:45 -0500

    test re-enabling subscription recurrance checkbox in an attempt to resolve #80

--- front-end-pages/resources/GSuiteAPI.php
+++ front-end-pages/resources/GSuiteAPI.php
@@ -76,7 +76,7 @@ function userFactory($username, $email, $firstName, $lastName, $password, $strip
             'Subscription_Management' => array(
                 'Subscription_Type' => $subcriptionType,
                 'Subscription_Status' => $subscriptionStatus,
-                //'Subscription_Recurring' => $subscriptionRecurring,
+                'Subscription_Recurring' => $subscriptionRecurring,
                 'Subscription_Expiration' => $subscriptionExpiration,
                 'Stripe_ID' => $stripeToken
                 ) 
