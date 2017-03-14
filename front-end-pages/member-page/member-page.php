169ef834bf2e95c569d022c4330f240858a53187
Thomas Coe <thomascoe1@gmail.com>
2017-03-14 13:08:31 -0400

    Remove extra body tag, add footer and sidebar

--- front-end-pages/member-page/member-page.php
+++ front-end-pages/member-page/member-page.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
 
-<?php include 'member-page.html';?>
-
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
-
+        <?php include 'member-page.html';?>
     </main>
 </div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
