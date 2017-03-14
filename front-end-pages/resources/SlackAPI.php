564480f66d18f8846d99c728b70fc1e4c5525a6c
Thomas Coe <thomascoe1@gmail.com>
2017-03-13 22:38:10 -0400

    Fix formatting (remove sloppy indentation, change mix of tabs and spaces to all spaces)

--- front-end-pages/resources/SlackAPI.php
+++ front-end-pages/resources/SlackAPI.php
@@ -8,26 +8,26 @@ define('SLACK_TOKEN_PATH', '/home/ubuntu/slack.txt');
 
 
 function getSlackToken() {
-	return trim(file_get_contents(SLACK_TOKEN_PATH));
+    return trim(file_get_contents(SLACK_TOKEN_PATH));
 }
 
 
 function sendSlackInvite($email, $firstName, $lastName) {
 
-	$token = getSlackToken();
-	$url = "https://slack.com/api/users.admin.invite?token=$token&email=$email&first_name=$firstName&last_name=$lastName";
-    
-  $args = array(
-    'timeout'     => 30,
-    'redirection' => 10,
-    'httpversion' => '1.1',
-  );
+    $token = getSlackToken();
+    $url = "https://slack.com/api/users.admin.invite?token=$token&email=$email&first_name=$firstName&last_name=$lastName";
 
-	$response = wp_remote_get($url, $args);
-	
-  $code = wp_remote_retrieve_response_code($response);
+    $args = array(
+        'timeout'     => 30,
+        'redirection' => 10,
+        'httpversion' => '1.1',
+    );
 
-	if ($code >= 300) {
-	  error_log("Error sending Slack Invite - HTTP Error:" . $code);
-	}
+    $response = wp_remote_get($url, $args);
+
+    $code = wp_remote_retrieve_response_code($response);
+
+    if ($code >= 300) {
+        error_log("Error sending Slack Invite - HTTP Error:" . $code);
+    }
 }
