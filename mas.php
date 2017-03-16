cce84bf4ae137ed4cd9126a5f8c5517be7386765
Craig Owenby <craig.owenby@gatech.edu>
2017-03-16 15:29:08 -0400

    Removed iFrame sandboxing and plugin enqueuing to attempt to fix issues.

--- mas.php
+++ mas.php
@@ -78,8 +78,42 @@ add_action( 'wp', 'elegance_referal_init' );
 
 require plugin_dir_path( __FILE__ ) . 'includes/class-makerspace-automation-suite.php';
 
-// Enqueueing the MAS styles
+// Enqueueing the MAS styles on the user profile and registration pages
 function mas_enqueue_styles() {
+    if (is_page('member') || is_page('register')) {
+        // Bootstrap
+        wp_enqueue_style( 'bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
+
+        // Angular Material
+        wp_enqueue_style( 'angular-material', '//ajax.googleapis.com/ajax/libs/angular_material/1.1.0/angular-material.min.css');
+    }
+}
+
+// Enqueueing the MAS scripts on the user profile and registration pages
+function mas_enqueue_scripts() {
+    if (is_page("member") || is_page('register')) {
+        // Bootstrap
+        wp_enqueue_script('bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
+
+        // Angular
+        wp_enqueue_script('angular', '//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js');
+        wp_enqueue_script('angular-animate', '//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-animate.min.js');
+        wp_enqueue_script('angular-aria', '//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-aria.min.js');
+        wp_enqueue_script('angular-messages', '//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-messages.min.js');
+        wp_enqueue_script('angular-material', '//ajax.googleapis.com/ajax/libs/angular_material/1.1.0/angular-material.min.js');
+    }
+}
+
+add_action('wp_enqueue_scripts', 'mas_enqueue_styles');
+add_action('wp_enqueue_scripts', 'mas_enqueue_scripts');
+
+// Enqueueing the MAS styles on the admin pages
+function mas_admin_enqueue_styles($hook) {
+    // Load only on ?page=mas-plugin
+    if($hook != 'toplevel_page_mas-plugin') {
+        return;
+    }
+    
     // Bootstrap
     wp_enqueue_style( 'bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
     
@@ -87,8 +121,13 @@ function mas_enqueue_styles() {
     wp_enqueue_style( 'angular-material', '//ajax.googleapis.com/ajax/libs/angular_material/1.1.0/angular-material.min.css');
 }
 
-// Enqueueing the MAS scripts
-function mas_enqueue_scripts() {
+// Enqueuing the MAS scripts on the admin pages
+function mas_admin_enqueue_scripts($hook) {
+    // Load only on ?page=mas-plugin
+    if($hook != 'toplevel_page_mas-plugin') {
+        return;
+    }
+
     // Bootstrap
     wp_enqueue_script('bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
 
@@ -99,9 +138,9 @@ function mas_enqueue_scripts() {
     wp_enqueue_script('angular-messages', '//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-messages.min.js');
     wp_enqueue_script('angular-material', '//ajax.googleapis.com/ajax/libs/angular_material/1.1.0/angular-material.min.js');
 }
+add_action('admin_enqueue_scripts', 'mas_admin_enqueue_styles');
+add_action('admin_enqueue_scripts', 'mas_admin_enqueue_scripts');
 
-add_action('admin_enqueue_scripts', 'mas_enqueue_styles');
-add_action('admin_enqueue_scripts', 'mas_enqueue_scripts');
 
 add_action('admin_menu', 'mas_admin_menu_setup');
 
