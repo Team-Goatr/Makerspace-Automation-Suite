773fc28248299053fe15846b65224299386f27dd
Thomas Coe <thomascoe1@gmail.com>
2017-03-28 21:26:31 -0400

    bugfix

--- resources/stripehook.php
+++ resources/stripehook.php
@@ -1,4 +1,6 @@
 <?php
+require_once __DIR__ . '../front-end-pages/resources/StripeAPI.php';
+
 Stripe\Stripe::setApiKey("sk_test_49SSSUMxAPbpYSmy4Omblrgk");
 
 $input = @file_get_contents("php://input");
