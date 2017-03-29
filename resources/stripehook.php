a0afcd559f63c1d4f984b8b4f26264d696f85bfd
Thomas Coe <thomascoe1@gmail.com>
2017-03-28 21:49:05 -0400

    bugfix

--- resources/stripehook.php
+++ resources/stripehook.php
@@ -2,13 +2,16 @@
 error_log("Webhook event received");
 
 #require_once __DIR__ . '/../front-end-pages/resources/StripeAPI.php';
-
 #Stripe\Stripe::setApiKey("sk_test_49SSSUMxAPbpYSmy4Omblrgk");
 
 $input = @file_get_contents("php://input");
 $event = json_decode($input);
-error_log($event->id);
+
+if ($event->type == 'invoice.payment_failed') {
+    $cus = $event->data->object->customer;
+    error_log("Payment failed for customer $cus");
+}
 
 
-http_response_code(400);
+http_response_code(200);
 ?>
