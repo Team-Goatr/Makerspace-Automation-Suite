2543bf773392a4cbc72d3996df1298e1004493d3
Thomas Coe <thomascoe1@gmail.com>
2017-03-28 21:37:08 -0400

    bugfix

--- resources/stripehook.php
+++ resources/stripehook.php
@@ -1,11 +1,13 @@
 <?php
 require_once __DIR__ . '/../front-end-pages/resources/StripeAPI.php';
 
-Stripe\Stripe::setApiKey("sk_test_49SSSUMxAPbpYSmy4Omblrgk");
+#Stripe\Stripe::setApiKey("sk_test_49SSSUMxAPbpYSmy4Omblrgk");
 
 $input = @file_get_contents("php://input");
 $event = json_decode($input);
 
+error_log("Webhook event received");
 error_log($event->id);
+
 http_response_code(200);
 ?>
