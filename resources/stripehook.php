5b7b9adc5efc9d24141c9e7215202bd49b8c3d44
Thomas Coe <thomascoe1@gmail.com>
2017-03-28 21:58:51 -0400

    bugfix

--- resources/stripehook.php
+++ resources/stripehook.php
@@ -1,15 +1,24 @@
 <?php
 error_log("Webhook event received");
 
+# TODO: Troubleshoot loading Stripe API
 #require_once __DIR__ . '/../front-end-pages/resources/StripeAPI.php';
 #Stripe\Stripe::setApiKey("sk_test_49SSSUMxAPbpYSmy4Omblrgk");
 
+# Get POST json data
 $input = @file_get_contents("php://input");
 $event = json_decode($input);
 
+#TODO: Get the event from Stripe and verify validity
+#\Stripe\Event::retrieve($event->id);
+
+# Note: event types defined at: https://stripe.com/docs/api#event_types
 if ($event->type == 'invoice.payment_failed') {
     $cus = $event->data->object->customer;
     error_log("Payment failed for customer $cus");
+    # TODO: Send email to admin and customer? Update G Suite
+} elseif ($event->type == 'invoice.payment_succeeded') {
+    # TODO: Update G suite subscription expiration date
 }
 
 
