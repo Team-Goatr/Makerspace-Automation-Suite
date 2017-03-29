cb3cf86793a3ba45c963b44aebcf7dd116f42057
Thomas Coe <thomascoe1@gmail.com>
2017-03-28 21:34:12 -0400

    bugfix

--- resources/stripehook.php
+++ resources/stripehook.php
@@ -6,7 +6,6 @@ Stripe\Stripe::setApiKey("sk_test_49SSSUMxAPbpYSmy4Omblrgk");
 $input = @file_get_contents("php://input");
 $event = json_decode($input);
 
-echo "success";
-echo $event->id;
+error_log($event->id);
 http_response_code(200);
 ?>
