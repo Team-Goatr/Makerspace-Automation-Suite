9cd892744006ee52acd0c44a97bb05e07ca74969
Ryan Strat <ryanstrat@users.noreply.github.com>
2017-03-31 04:51:53 -0400

    start work admin-member-edit

---
+++ postable/admin-member-edit.php
@@ -0,0 +1,27 @@
+<?php
+
+defined( 'ABSPATH' ) or die();
+
+include_once dirname(__DIR__).'../../../resources/GSuiteAPI.php';
+
+// Adding the keys to the wordpress hooks
+add_action('admin_post_update_member', 'prefix_admin_update_member');
+
+function prefix_admin_update_member() {
+    $username = $_POST['username'];
+
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+
+    $properties = array(
+    	'name' => array(
+    		'givenName' => $firstName,
+            'familyName' => $lastName
+    	)
+    );
+
+    updateUser($username, $properties);
+
+    wp_redirect(admin_url('admin.php?page=mas-plugin'));
+    exit();
+}
