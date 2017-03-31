d5012053a03235353c99e428775204215f8139b0
Ryan Strat <ryanstrat@users.noreply.github.com>
2017-03-31 05:00:18 -0400

    debug

--- postable/admin-member-edit.php
+++ postable/admin-member-edit.php
@@ -3,11 +3,12 @@
 defined( 'ABSPATH' ) or die();
 
 include_once dirname(__DIR__).'../../../resources/GSuiteAPI.php';
-define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
 // Adding the keys to the wordpress hooks
 add_action('admin_post_update_member', 'prefix_admin_update_member');
 
 function prefix_admin_update_member() {
+    echo "Function Called";
     $username = $_POST['username'];
 
     $firstName = $_POST['firstName'];
