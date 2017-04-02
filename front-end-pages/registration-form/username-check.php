91a7f6f489914c9c64892941a39712fe28c69f79
Ryan Strat <ryanstrat@users.noreply.github.com>
2017-04-02 15:42:33 -0400

    dev

--- front-end-pages/registration-form/username-check.php
+++ front-end-pages/registration-form/username-check.php
@@ -5,14 +5,13 @@ require_once dirname(__DIR__).'/resources/GSuiteAPI.php';
 if (isset($_GET["username"])) {
     $username_to_check = $_GET["username"];
 
-/*
+
     try {
     	getUser($username_to_check); //Will fail if username is not in use
     	$result = false; //Username is taken
     } catch(Exception $e) {
     	$result = true; //username is available
     }
-*/
-    $result = false;
+
     echo '{ result: ' + $result + '}';
 }
