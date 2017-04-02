1625d2beab675d1435e96dd5b0dc86c02b61b7f1
Ryan Strat <ryanstrat@users.noreply.github.com>
2017-04-02 16:28:09 -0400

    dev

--- front-end-pages/registration-form/username-check.php
+++ front-end-pages/registration-form/username-check.php
@@ -6,17 +6,13 @@ if (isset($_GET["username"])) {
     $username_to_check = $_GET["username"];
 
     $email = $username_to_check . "@decaturmakers.org";
-    error_log("Username to check: ". $email);
 
     try {
     	getUser($email); //Will fail if username is not in use
     	$result = "false"; //Username is taken
-    	error_log("end of Try");
     } catch(Exception $e) {
     	$result = "true"; //username is available
     }
-
-    error_log("Result: " . $result);
 
     echo '{ result: ' . $result . '}';
 }
