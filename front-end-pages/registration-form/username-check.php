fc5c366f86c2970630d0210c6778116f8cff82e3
Thomas Coe <thomascoe1@gmail.com>
2017-04-02 17:47:54 -0400

    Duplicate username check (#105)
    * new member view page added to the front end
    
    * Bootstrap integrations page
    
    * Fix background color on updated integrations page
    
    * Bootstrap member table
    
    * Center column headers
    
    * style test
    
    * Done with bootstrap styling for member table
    
    * Member Edit page for makerspace memebrs complete UI
    
    * Fixed Stripe and GSuite APIs being included more than once on integrations page
    
    * Integrations bugfix: use admin-post.php. Also fixed indentation consistency and removed trailing whitespace
    
    * Fix indentations and trailing whitespace
    
    * Fixed style. Removed extra html and body tags
    
    * Remove extra body tag, add footer and sidebar
    
    * remove sidebar
    
    * Add back body tag
    
    * change body tag to div
    
    * test php change
    
    * Testing proper method for enqueueing styles and scripts
    
    * Bugfixes
    
    * Bugfixes
    
    * test uncomment of html
    
    * Add Stripe integration to member page
    
    * bugfix
    
    * add card brand
    
    * Remove card update from admin page
    
    * Add slack php
    
    * Testing iFrame on Member Page for Theme Sandboxing
    
    * Removed iFrame sandboxing and plugin enqueuing to attempt to fix issues.
    
    * Added jquery dependency to bootstrap scripts
    
    * Testing removal of Bootstrap CSS
    
    * Canceled removal
    
    * Bugfixes
    
    * Fixed bootstrap background color override
    
    * Fixed bootstrap background color override with proper JQuery usage
    
    * Add password fields to regristration page
    
    * Add error checking
    
    * Error checking ffix
    
    * js fix
    
    * Error checking
    
    * Error checking
    
    * Load js
    
    * do not load js
    
    * actually loading angular
    
    * Forgot to specify dependencies
    
    * Fix variable name
    
    * Change password requirements
    
    * fix mispelling
    
    * Test
    
    * Test
    
    * Include ng-messages
    
    * Test
    
    * Add member edit fields
    
    * Image fix
    
    * image fix
    
    * responsive
    
    * Test
    
    * Put contents into table
    
    * Add space between fields
    
    * Whoops, remove extra row
    
    * styling got messed up
    
    * Found the issue - active CSS
    
    * back to basics
    
    * New grid
    
    * Update button class
    
    * CSS fix
    
    * CSS Cleanup
    
    * Ignoring the breadcrumb styling issues
    
    * Possible CSS fix
    
    * Try again
    
    * test
    
    * Make membership plan a dropdown
    
    * make subscription exp date a date
    
    * fix breadcrumb
    
    * fix background color
    
    * background color
    
    * Beginning work on frontend for username check
    
    * Beginning work on frontend for username check
    
    * Beginning work on frontend for username check
    
    * Beginning work on frontend for username check
    
    * Beginning work on frontend for username check
    
    * Beginning work on frontend for username check
    
    * Beginning work on frontend for username check
    
    * Beginning work on frontend for username check
    
    * Beginning work on frontend for username check
    
    * Beginning work on frontend for username check
    
    * Beginning work on frontend for username check
    
    * Beginning work on frontend for username check
    
    * Beginning work on frontend for username check
    
    * Beginning work on frontend for username check
    
    * Beginning work on frontend for username check
    
    * Finished work on username check. The file that needs to have the final check code is '/front-end-pages/registration-form/username-check.php'
    
    * Initial implementation of backend
    
    * semicolon
    
    * syntax
    
    * dependency
    
    * dependency
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * Fixes #72
    
    * consistency

---
+++ front-end-pages/registration-form/username-check.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once dirname(__DIR__).'/resources/GSuiteAPI.php';
+
+if (isset($_GET["username"])) {
+    $username_to_check = $_GET["username"];
+
+    $email = $username_to_check . "@decaturmakers.org";
+
+    try {
+    	getUser($email); //Will fail if username is not in use
+    	$result = "false"; //Username is taken
+    } catch(Exception $e) {
+    	$result = "true"; //username is available
+    }
+
+    echo '{ "result": ' . $result . '}';
+}
