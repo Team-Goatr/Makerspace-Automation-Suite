652e440f9344d22419972f84f8548919c754ea1e
Thomas Coe <thomascoe1@gmail.com>
2017-04-02 14:45:34 -0400

    Get all users from G Suite

--- resources/rfidhook.php
+++ resources/rfidhook.php
@@ -3,4 +3,19 @@ defined( 'ABSPATH' ) or die();
 
 function rfid_webhook_listener() {
     error_log("RFID webhook event received");
+    require_once __DIR__ . '/../front-end-pages/resources/GSuiteAPI.php';
+
+    $service = getService();
+    $optParams = array(
+        'domain' => 'decaturmakers.org',
+        'projection' => 'custom',
+        'customFieldMask' => 'Subscription_Management,roles',
+        'orderBy' => 'email'
+    );
+    $results = $service->users->listUsers($optParams);
+    $users = $results->getUsers();
+    var_dump($users);
+
+    http_response_code(200);
+    die();
 }
